377704c4b35cf54b3d863ac00fe76ecd172cc7be
Ryan McClean <${USER_EMAIL}>
2025-12-03 16:38:24 +0000

    Add projects for days two to twelve

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2025
+
+## Trying in C# this year, it's what I work in currently so it's good practice
--- two/Program.cs
+++ two/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     private static class Globals
     {
-        public int invalidCount = 0;
+        public static int invalidCount = 0;
     }
 
     public static void Main(string[] args)
     {
         Console.WriteLine("Day Two");
     }
-    
+
 }
